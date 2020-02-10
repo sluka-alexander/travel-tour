72591edb121c749efce27935bc0b65f8470cb962
Sluka Alexander <slukasane@gmail.com>
2020-02-10 23:17:46 +0300

    validate

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
+
 
 router.get('/', (req, res) => {
     res.render('welcome');
@@ -16,10 +18,44 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', (req, res) => {
-    res.send(req.body);
+    const { name, email, password } = req.body;
+    let errors = [];
+
+    // const input = document.getElementsByTagName('input');
+    // input.value = localStorage.getItem('input');
+    // input.oninput = () => {
+    //     localStorage.setItem('input', input.value)
+    // };
+
+    if(!name || !email || !password) {
+        errors.push({msg: 'Please fill in all fields'})
+    }
+    if(errors.length > 0){
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password
+        });
+    }
+    else {
+        res.send(req.body);
+        let user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+        user.save()
+            .then(function(doc){
+                console.log("saved user", doc);
+                res.sendStatus(200);
+                mongoose.disconnect();
+            })
+            .catch(function (err){
+                console.log(err);
+                mongoose.disconnect();
+            });
+    }
 });
-
-
-
 
 module.exports = router;
